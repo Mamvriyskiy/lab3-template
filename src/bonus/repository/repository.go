7d669afd838e2f84e29fa43c8f69537674c40335
Mamvriyskiy <68384944+Mamvriyskiy@users.noreply.github.com>
2025-11-14 20:10:32 +0300

    Обновил

---
+++ src/bonus/repository/repository.go
@@ -0,0 +1,22 @@
+package repository
+
+import (
+	model "github.com/Mamvriyskiy/lab3-template/src/bonus/model"
+	"github.com/jmoiron/sqlx"
+)
+
+type RepoBonus interface {
+	GetInfoAboutUserPrivilege(username string) (model.PrivilegeResponse, error)
+	UpdateBonus(username, ticketUID string, price int) (model.PrivilegeInfo, error)
+	UpdateBonusBonus(username, ticketUid string, price int) error
+}
+
+type Repository struct {
+	RepoBonus
+}
+
+func NewRepository(db *sqlx.DB) *Repository {
+	return &Repository{
+		RepoBonus: NewBonusPostgres(db),
+	}
+}
